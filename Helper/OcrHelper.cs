e285e3346b9bd48e6e2d633831d9fa42d22b0ad4
vuuu109 <103552957+vuuu109@users.noreply.github.com>
2026-07-10 02:23:46 +0800

    Fix OcrHelper.cs - method inside namespace

--- Helper/OcrHelper.cs
+++ Helper/OcrHelper.cs
@@ -715,7 +715,6 @@ namespace TrOCR.Helper
             }
         }
     }
-}
         public static async System.Threading.Tasks.Task<string> Pix2Text(byte[] imageBytes)
         {
             return await System.Threading.Tasks.Task.Run(() =>
